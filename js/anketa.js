1160b8922b146441905bc601af861b261934eeef
Karter13 <KarMiH1975@mail.ru>
2019-12-03 21:25:16 +0300

    fix(hw1#1): fix anketa.js

--- js/anketa.js
+++ js/anketa.js
@@ -1,32 +1,25 @@
 
-let userSurname = prompt('Ваше фамилия?', '');
+let userSurname = prompt('Ваша фамилия?', '');
 let userName = prompt('Ваше имя?', '');
 let userPatronymic = prompt('Ваше отчество?', '');
 let userAge = +prompt('Сколько Вам полных лет?', '');
 const userSex = confirm('Ваш пол мужской?');
 const userSexRezalt = (userSex) ? 'мужской' : 'женский';
-let pensionAnswer;
+const pensionAnswer = userAge >= 63 ? 'Да' : 'Нет';
 
-while (userSurname.length < 2
-  || userSurname.length > 25
-  || userSurname === ''
-  || userSurname === null) {
-  userSurname = prompt('Ваше фамилия?', '');
+function checkUserNames(value, message) {
+  let result = value;
+  while (result.length < 2
+    || result.length > 25
+    || result === null) {
+    result = prompt(message, '');
+  }
+  return result;
 }
 
-while (userName.length < 2
-  || userName.length > 25
-  || userName === ''
-  || userSurname === null) {
-  userName = prompt('Ваше имя?', '');
-}
-
-while (userPatronymic.length < 2
-  || userPatronymic.length > 25
-  || userPatronymic === ''
-  || userSurname === null) {
-  userPatronymic = prompt('Ваше отчество?', '');
-}
+userSurname = checkUserNames(userSurname, 'Ваша фамилия?');
+userName = checkUserNames(userName, 'Ваше имя?');
+userPatronymic = checkUserNames(userPatronymic, 'Ваше отчество?');
 
 while (isNaN(userAge)
   || userAge === ''
@@ -34,12 +27,6 @@ while (isNaN(userAge)
   || userAge < 0
   || userAge > 100) {
   userAge = +prompt('Сколько Вам полных лет?', '');
-}
-
-if (userAge >= 63) {
-  pensionAnswer = 'Да';
-} else {
-  pensionAnswer = 'Нет';
 }
 
 alert(`ваше ФИО: ${userSurname} ${userName} ${userPatronymic} 
