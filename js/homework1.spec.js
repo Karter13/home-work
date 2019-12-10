7c2ccd3ebf0f438ea1bb9bfd399c3901dde94fb3
Karter13 <KarMiH1975@mail.ru>
2019-12-10 19:53:34 +0300

    feat(hw2#6): make tests for functions  homework1.js

--- js/homework1.spec.js
+++ js/homework1.spec.js
@@ -7,8 +7,8 @@ describe('homework1.js -> isPolindrom1 -> should return true or false', function
 
 describe('homework1.js -> isPolindrom2 -> should return true or false', function () {
   it('Base test', function () {
-    expect(isPolindrom2('dffd')).toBe(true);
-    expect(isPolindrom2('fhjjjjj')).toBe(false);
+    expect(isPolindrom2('dffd')).toBeTruthy();
+    expect(isPolindrom2('fhjjjjj')).toBeFalsy();
   });
 });
 
@@ -19,4 +19,26 @@ describe('homework1.js -> isPolindrom3 -> should return \'yes\' or \'no\'  ', fu
   });
 });
 
+describe('homework1.js -> getNumWord -> should return right word', function () {
+  it('Basic tasts', function () {
+    expect(getNumWord('1', 'яблоко', 'яблока', 'яблок')).toBe('яблоко');
+    expect(getNumWord('123', 'яблоко', 'яблока', 'яблок')).toBe('яблока');
+    expect(getNumWord('113', 'яблоко', 'яблока', 'яблок')).toBe('яблок');
+  });
+});
 
+describe('test.js -> makeTest; -> test prompt', function () {
+
+  it('should return string', function () {
+    spyOn(window, 'prompt').and.callFake(function () {
+      let result;
+      let num = 1;
+      if (num) {
+        result = 'bb';
+      }
+      return result;
+    });
+    let result = makeTest();
+    expect(result).toBe(false);
+  });
+});
