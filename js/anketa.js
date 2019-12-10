65735ed8f27e39ebe0c8df694dc85051b870c037
Karter13 <KarMiH1975@mail.ru>
2019-12-10 20:18:56 +0300

    feat(hw2#8): make rerefactoring anketa.js

--- js/anketa.js
+++ js/anketa.js
@@ -1,37 +1,55 @@
+// let userSurname = prompt('Ваша фамилия?', '');
+// let userName = prompt('Ваше имя?', '');
+// let userPatronymic = prompt('Ваше отчество?', '');
+// let userAge = +prompt('Сколько Вам полных лет?', '');
+// const userSex = confirm('Ваш пол мужской?');
 
-let userSurname = prompt('Ваша фамилия?', '');
-let userName = prompt('Ваше имя?', '');
-let userPatronymic = prompt('Ваше отчество?', '');
-let userAge = +prompt('Сколько Вам полных лет?', '');
-const userSex = confirm('Ваш пол мужской?');
-const userSexRezalt = (userSex) ? 'мужской' : 'женский';
-const pensionAnswer = userAge >= 63 ? 'Да' : 'Нет';
 
 function checkUserNames(value, message) {
   let result = value;
   while (result.length < 2
     || result.length > 25
     || result === null) {
+    // eslint-disable-next-line no-alert
     result = prompt(message, '');
   }
   return result;
 }
+// userSurname = checkUserNames(userSurname, 'Ваша фамилия?');
+// userName = checkUserNames(userName, 'Ваше имя?');
+// userPatronymic = checkUserNames(userPatronymic, 'Ваше отчество?');
 
-userSurname = checkUserNames(userSurname, 'Ваша фамилия?');
-userName = checkUserNames(userName, 'Ваше имя?');
-userPatronymic = checkUserNames(userPatronymic, 'Ваше отчество?');
 
-while (isNaN(userAge)
-  || userAge === ''
-  || userAge === null
-  || userAge < 0
-  || userAge > 100) {
-  userAge = +prompt('Сколько Вам полных лет?', '');
+function getFullUserAge(value, message) {
+  let fullUserAge = value;
+  while (isNaN(fullUserAge)
+    || fullUserAge === ''
+    || fullUserAge === null
+    || fullUserAge < 0
+    || fullUserAge > 100) {
+    fullUserAge = +prompt(message);
+  }
+  return fullUserAge;
+}
+// userAge = getFullUserAge(userAge, 'Сколько Вам полных лет?');
+
+
+function getUserssex(value) {
+  return (value) ? 'мужской' : 'женский';
 }
 
-alert(`ваше ФИО: ${userSurname} ${userName} ${userPatronymic} 
-ваш возраст в годах: ${userAge}
-ваш возраст в днях: ${365 * userAge}
-через 5 лет вам будет: ${userAge + 5}
-ваш пол: ${userSexRezalt}
-вы на пенсии: ${pensionAnswer}`);
+
+function getAnswerAboutPension(age, sex) {
+  // eslint-disable-next-line no-mixed-operators
+  if (sex && age >= 63 || !sex && age >= 58) {
+    return 'Да!';
+  }
+  return 'Нет!';
+}
+
+// alert(`ваше ФИО: ${userSurname} ${userName} ${userPatronymic} 
+// ваш возраст в годах: ${userAge}
+// ваш возраст в днях: ${365 * userAge}
+// через 5 лет вам будет: ${userAge + 5}
+// ваш пол: ${getUserssex(userSex)}
+// вы на пенсии: ${getAnswerAboutPension(userAge, userSex)}`);
