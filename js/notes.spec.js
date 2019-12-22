4bc507f41056e50f48181460ca6bd83174fd975a
Karter13 <KarMiH1975@mail.ru>
2019-12-22 22:41:25 +0300

    fix(hw3#4): clear note.spec.js

--- js/notes.spec.js
+++ js/notes.spec.js
@@ -1,6 +1,2 @@
-describe('notes.js -> createListNotes -> should return Obj', function () {
-    it('should return Obj', function () {
-        expect(createListNotes('title Notes1', 'body Notes1')).toEqual({ title: 'title Notes1', body: 'body Notes1' })
-    });
-});
+
 
