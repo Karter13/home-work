d090d7865a33b667582bae0ab97cb4ca6fba7794
Karter13 <KarMiH1975@mail.ru>
2019-12-28 11:07:05 +0300

    feat(hw4#4): added functions cleanString() and UsersSort()

--- js/homework4.js
+++ js/homework4.js
@@ -36,3 +36,97 @@ const DayForBirthday = (function () {
     showHours(2019);
   });
 }());
+
+
+function cleanString(newString) {
+  let str = newString;
+
+  const cenzrBlock = {
+    конь: '*****',
+    столб: '*****',
+  };
+
+  for (const key in cenzrBlock) {
+    str = str.split(key).join(cenzrBlock[key]);
+  }
+  return str;
+}
+console.log(cleanString('Моя мама мыла столб, на улице стоит конь'));
+
+
+const Users = [
+  { id: '3', name: 'Alex', dob: '1999-01-24' },
+  { id: '1', name: 'Maikl', dob: '1975-10-07' },
+  { id: '2', name: 'anna', dob: '1977-06-09' },
+  { id: '4', name: 'Masha', dob: '2004-10-11' },
+];
+
+const UsersSort = (function () {
+  function sortNumber(param1, param3) {
+    if (param3 === 'asc') {
+      return param1.sort((a, b) => a.id - b.id);
+    }
+    if (param3 === 'desc') {
+      return param1.sort((a, b) => b.id - a.id);
+    }
+    return false;
+  }
+
+  function sortString(param1, param3) {
+    if (param3 === 'asc') {
+      return param1.sort((a, b) => {
+        const aname = a.name.toLowerCase();
+        const bname = b.name.toLowerCase();
+        if (aname > bname) return 1;
+        if (aname < bname) return -1;
+      });
+    }
+    if (param3 === 'desc') {
+      return param1.sort((a, b) => {
+        const aname = a.name.toLowerCase();
+        const bname = b.name.toLowerCase();
+        if (aname > bname) return -1;
+        if (aname < bname) return 1;
+      });
+    }
+    return false;
+  }
+
+  function sortDate(param1, param3) {
+    if (param3 === 'asc') {
+      return param1.sort((a, b) => {
+        const adate = new Date(a.dob);
+        const bdate = new Date(b.dob);
+        return adate - bdate;
+      });
+    }
+    if (param3 === 'desc') {
+      return param1.sort((a, b) => {
+        const adate = new Date(a.dob);
+        const bdate = new Date(b.dob);
+        return bdate - adate;
+      });
+    }
+    return false;
+  }
+
+  function chengeArray(param1, param2, param3) {
+    if (param2 === 'id') {
+      return sortNumber(param1, param3);
+    }
+    if (param2 === 'name') {
+      return sortString(param1, param3);
+    }
+    if (param2 === 'dob') {
+      return sortDate(param1, param3);
+    }
+    return false;
+  }
+
+  return {
+    users(param1, param2, param3) {
+      return chengeArray(param1, param2, param3);
+    },
+  };
+}());
+console.log(UsersSort.users(Users, 'id', 'asc'));
