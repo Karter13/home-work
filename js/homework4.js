3544e05262de1a2a8226ab37bc1dea8c68301c42
Karter13 <KarMiH1975@mail.ru>
2019-12-28 10:51:22 +0300

    feat(hw4#3: added functions calculateDifferenceTime, DayForBirthday

--- js/homework4.js
+++ js/homework4.js
@@ -1,1 +1,38 @@
-console.log('heloo');
+moment.locale('ru');
+
+const timer = document.querySelector('#timer');
+
+function calculateDifferenceTime() {
+  const dayNow = moment();
+  const dayEnd = moment().endOf('day');
+  const difference = dayEnd.diff(dayNow, 's');
+
+  timer.innerHTML = `до конца дня осталось ${difference} секунд`;
+}
+
+calculateDifferenceTime();
+setInterval(calculateDifferenceTime, 1000);
+
+const DayForBirthday = (function () {
+  const input = document.getElementById('birthday-date');
+  const daysToBirthday = document.getElementById('days-tobirthday');
+  const button = document.getElementById('show-days');
+
+  function showHours(thisYear) {
+    const nextYear = thisYear + 1;
+    const dateBirth = input.value;
+    const dayNow = moment();
+    let dayBirthday = moment(dateBirth).year(thisYear);
+
+    if (dayNow > dayBirthday) {
+      dayBirthday = moment(dateBirth).year(nextYear);
+    }
+
+    const difference = dayBirthday.diff(dayNow, 'd');
+    daysToBirthday.innerHTML = difference;
+  }
+
+  button.addEventListener('click', () => {
+    showHours(2019);
+  });
+}());
